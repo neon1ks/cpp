21f0eb02bd8d1ee086eebfec6be4546b94592f22
Maksim Demyanov (neon1ks) <neon1ks@mail.ru>
2020-03-04 14:17:24 +0800

    Add work-with-path project

---
+++ 2020_03_04_work-with-path/work-with-path/work-with-path.cpp
@@ -0,0 +1,12 @@
+﻿// work-with-path.cpp : Defines the entry point for the application.
+//
+
+#include "work-with-path.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
